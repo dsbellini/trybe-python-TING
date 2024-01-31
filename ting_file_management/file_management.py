484bcf543e011d4631ecbc3c2f81ff74bd86da2e
Danilo Bellini <bellini.daan@gmail.com>
2024-01-31 18:32:35 -0300

    feat: text importer implemented

--- ting_file_management/file_management.py
+++ ting_file_management/file_management.py
@@ -1,2 +1,17 @@
+import os
+import sys
+
+
 def txt_importer(path_file):
-    """Aqui irá sua implementação"""
+    file_format = os.path.splitext(path_file)
+
+    if file_format[1] == ".txt":
+        try:
+            with open(path_file, "r") as file:
+                file_content = file.read()
+                file_lines = file_content.split("\n")
+                return file_lines
+        except FileNotFoundError:
+            sys.stderr.write(f"Arquivo {path_file} não encontrado\n")
+    else:
+        sys.stderr.write("Formato inválido")
