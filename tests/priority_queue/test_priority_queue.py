36cfaac43218b986d18fcc49e00629ad038c341d
Danilo Bellini <bellini.daan@gmail.com>
2024-02-01 18:51:53 -0300

    feat: priority queue test implemented

--- tests/priority_queue/test_priority_queue.py
+++ tests/priority_queue/test_priority_queue.py
@@ -1,5 +1,25 @@
-# from ting_file_management.priority_queue import PriorityQueue
+import pytest
+from ting_file_management.priority_queue import PriorityQueue
 
 
 def test_basic_priority_queueing():
-    """Aqui irá sua implementação"""
+    # Criei a fila de prioridade
+    queue = PriorityQueue()
+
+    # Adicionei 2 elementos
+    queue.enqueue({"qtd_linhas": 8})
+    queue.enqueue({"qtd_linhas": 2})
+
+    # Verifiquei se a fila tem 2 elementos
+    assert len(queue) == 2
+    # Verifiquei se o primeiro elemento [0] é o de menor prioridade
+    assert queue.search(0) == {"qtd_linhas": 2}
+    assert queue.search(1) == {"qtd_linhas": 8}
+
+    # Removi o elemento menor e verifiquei se a fila tem 1 elemento
+    assert queue.dequeue() == {"qtd_linhas": 2}
+    assert len(queue) == 1
+
+    # Verifiquei se o erro é lançado ao tentar acessar um índice inválido
+    with pytest.raises(IndexError):
+        queue.search(1)
